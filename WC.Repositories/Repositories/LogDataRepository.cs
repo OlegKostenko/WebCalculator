52f9e38f79b94fbfcf53090bf254905c222b0350
Oleg Kostenko <pora93@gmail.com>
2019-05-07 13:42:08 +0300

    Add Repository

---
+++ WC.Repositories/Repositories/LogDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Reposirories.Common;
+using WC.DAL.DbLayer;
+
+namespace WC.Repositories.Repositories
+{
+    public class LogDataRepository : GenericRepository<LogData>
+    {
+        public LogDataRepository(DbContext context) : base(context) { }
+    }
+}
